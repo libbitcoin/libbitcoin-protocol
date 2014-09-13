45f347f5b1f9d5fff67e72b553980ee617d95778
Phillip Mienk <mienkphi@gmail.com>
2014-09-13 01:42:38 -0700

    Add java and python protocol buffer generation and scripts.

--- update_cpp.sh
+++ update_cpp.sh
@@ -6,16 +6,18 @@ pushd proto
 
 # generate c++ source/header
 echo "generating c++ sources"
-protoc --cpp_out=dllexport_decl=BCP_API:. bitcoin/protocol/interface.proto
+protoc --cpp_out=dllexport_decl=BCP_API:../cpp/include bitcoin/protocol/interface.proto
+
+popd
+pushd cpp
 
 # modify generated header to introduce define.hpp inclusion
 echo "modifying generated header"
-sed -i '/protoc_insertion_point(includes)/ i\#include <bitcoin\/protocol\/define.hpp>' bitcoin/protocol/interface.pb.h
+sed -i '/protoc_insertion_point(includes)/ i\#include <bitcoin\/protocol\/define.hpp>' ./include/bitcoin/protocol/interface.pb.h
 
 # relocate generated/modified files into c++ project
 echo "relocating files to 'cpp' directory"
-mv bitcoin/protocol/interface.pb.h ../cpp/include/bitcoin/protocol/
-mv bitcoin/protocol/interface.pb.cc ../cpp/src/
+mv ./include/bitcoin/protocol/interface.pb.cc ./src/
 
 popd
 
