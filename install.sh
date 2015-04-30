2b55d457a7ea4ea901ba2c2cbe3b9bb3a1f669fa
Phillip Mienk <mienkphi@gmail.com>
2015-04-29 19:26:24 -0700

    Regenerated from updated libbitcoin-build project.

--- install.sh
+++ install.sh
@@ -251,6 +251,7 @@ SECP256K1_OPTIONS=\
 #------------------------------------------------------------------------------
 BITCOIN_OPTIONS=\
 "--without-tests "\
+"--without-examples "\
 "${with_boost} "\
 "${with_pkgconfigdir} "
 
