afacb14712c226b567d697919d7c979c63660f91
Agustin K-ballo Berge <k@fusionfenix.com>
2016-09-09 10:21:30 -0300

    Adapt to latest FindProtobuf.cmake

--- cmake/protobuf_generate_cpp.cmake
+++ cmake/protobuf_generate_cpp.cmake
@@ -7,10 +7,16 @@ set(_proto_source "${_proto_we}.pb.cc")
 set(_proto_header "${_proto_we}.pb.h")
 
 execute_process(
-  COMMAND ${PROTOBUF_PROTOC_EXECUTABLE}
+  COMMAND ${Protobuf_PROTOC_EXECUTABLE}
     --cpp_out dllexport_decl=BCP_API:${CMAKE_CURRENT_SOURCE_DIR}
     --proto_path ${PROTO_PATH}
-    ${PROTO_FILE} )
+    ${PROTO_FILE}
+  RESULT_VARIABLE _result )
+
+if (NOT _result EQUAL 0)
+  message(FATAL_ERROR "Error processing ${PROTO_FILE} (${_result})")
+  return()
+endif()
 
 # modify generated header to introduce define.hpp inclusion
 file(READ ${_proto_header} _proto_header_contents)
