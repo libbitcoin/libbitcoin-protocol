3b1549d68c334c837636fc5130be1d2dd3c243cb
evoskuil <eric@voskuil.org>
2017-02-09 14:22:14 -0800

    Update version3 branch installer to build from version3 branches.

--- install.sh
+++ install.sh
@@ -671,8 +671,8 @@ build_all()
     build_from_tarball_boost $BOOST_URL $BOOST_ARCHIVE bzip2 . $PARALLEL "$BUILD_BOOST" "${BOOST_OPTIONS[@]}"
     build_from_github zeromq libzmq master $PARALLEL ${ZMQ_OPTIONS[@]} "$@"
     build_from_github libbitcoin secp256k1 version4 $PARALLEL ${SECP256K1_OPTIONS[@]} "$@"
-    build_from_github libbitcoin libbitcoin master $PARALLEL ${BITCOIN_OPTIONS[@]} "$@"
-    build_from_travis libbitcoin libbitcoin-protocol master $PARALLEL ${BITCOIN_PROTOCOL_OPTIONS[@]} "$@"
+    build_from_github libbitcoin libbitcoin version3 $PARALLEL ${BITCOIN_OPTIONS[@]} "$@"
+    build_from_travis libbitcoin libbitcoin-protocol version3 $PARALLEL ${BITCOIN_PROTOCOL_OPTIONS[@]} "$@"
 }
 
 
