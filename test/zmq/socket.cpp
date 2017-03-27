1831898df9db71b1689a7c7d605930b44c161c3a
evoskuil <eric@voskuil.org>
2017-03-27 16:59:57 -0700

    Add socket test cases.

--- test/zmq/socket.cpp
+++ test/zmq/socket.cpp
@@ -18,12 +18,60 @@
  */
 #include <boost/test/test_tools.hpp>
 #include <boost/test/unit_test_suite.hpp>
+
+#include "../utility.hpp"
 #include <bitcoin/protocol.hpp>
+
+using namespace bc;
+using namespace bc::protocol;
+using role = zmq::socket::role;
 
 BOOST_AUTO_TEST_SUITE(socket_tests)
 
-BOOST_AUTO_TEST_CASE(socket_test)
+BOOST_AUTO_TEST_CASE(socket__push_pull__shared_context__received)
 {
+    zmq::context context;
+    BOOST_REQUIRE(context);
+
+    zmq::socket pusher(context, role::pusher);
+    BOOST_REQUIRE(pusher);
+    BC_REQUIRE_SUCCESS(pusher.bind({ TEST_URL }));
+
+    zmq::socket puller(context, role::puller);
+    BOOST_REQUIRE(puller);
+    BC_REQUIRE_SUCCESS(puller.connect({ TEST_URL }));
+
+    SEND_MESSAGE(pusher);
+    WAIT_SUCCESS(puller);
+    RECEIVE_MESSAGE(puller);
+}
+
+BOOST_AUTO_TEST_CASE(socket__push_pull__secure_server_secure_client__blocked)
+{
+    zmq::certificate server_certificate;
+    BOOST_REQUIRE(server_certificate);
+
+    zmq::certificate client_certificate;
+    BOOST_REQUIRE(client_certificate);
+
+    zmq::context context;
+    BOOST_REQUIRE(context);
+
+    zmq::socket pusher(context, role::pusher);
+    BOOST_REQUIRE(pusher);
+    BOOST_REQUIRE(pusher.set_private_key(server_certificate.private_key()));
+    BOOST_REQUIRE(pusher.set_curve_server());
+    BC_REQUIRE_SUCCESS(pusher.bind({ TEST_URL }));
+
+    zmq::socket puller(context, role::puller);
+    BOOST_REQUIRE(puller);
+    BOOST_REQUIRE(puller.set_curve_client(server_certificate.public_key()));
+    BOOST_REQUIRE(puller.set_certificate(client_certificate));
+    BC_REQUIRE_SUCCESS(puller.connect({ TEST_URL }));
+
+    // There is no ZAP service running, so this fails despite configuration.
+    SEND_MESSAGE(pusher);
+    WAIT_FAILURE(puller);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
