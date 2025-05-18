ab11991c761867836dcd7970fdc6d86404322398
evoskuil <eric@voskuil.org>
2025-05-18 00:29:40 -0400

    Whitespace.

--- include/bitcoin/protocol/config/authority.hpp
+++ include/bitcoin/protocol/config/authority.hpp
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #ifndef LIBBITCOIN_PROTOCOL_CONFIG_AUTHORITY_HPP
 #define LIBBITCOIN_PROTOCOL_CONFIG_AUTHORITY_HPP
 
